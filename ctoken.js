165f5aacde53c1cd6ba643caf111a40135c00e00
VladKovalevskii <vladimir.a.kovalevskiy@gmail.com>
2018-07-04 19:00:45 +0300

    cleaning up

--- ctoken.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  CoreToken: '0xa57392548087453dec6106e670bbfb849276b358',
-  Collector: '0x3a1116594eecf36d8811469eb97a2b2ccb7cfd97',
-  Issuer: '0x74cc97acd573492a1408828d2b30fee3eb1d214a',
-  Cashier: '0x9433e5069a5fd4f2aa6384db9892c948cfa09aac',
-  Trader: '0x64f2cf5ec9e5bd739d50f07004cb45e671fd3ac5',
-  Owner: '0x6e617119909b5a03dc381c16eb68846F6cB458C1'
-}
